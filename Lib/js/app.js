d1bba98bb42af847822202a7c1fab4d84e31e412
Mohammed M Ahmed <m@rinum.com>
2017-03-13 13:17:31 -0500

    Update app.js
    Added microphone support

--- Lib/js/app.js
+++ Lib/js/app.js
@@ -3,12 +3,27 @@ TextRPG = function(){
 };
 
 TextRPG.prototype.init = function(){
+	var This = this;
+	
 	if(typeof Scenario === "undefined"){
 		alert('Put this before <!-- Load Scripts --> in index.html: \n\n <script type="text/javascript" src="./Scenarios/foobar.js"></script> \n\n and replace foobar with the desired scenario.');
 		return;
 	}
 
 	this.stack = [];
+	
+	this.noise = 0;
+	
+	this.mic = new Microphone({
+		unit: .5,
+		overlap: .5
+	}, function (data) {
+		v = data.map(function (d) { return d * d; });
+		v = v.reduce(function (t, s) { return t + s; });
+		v = Math.sqrt(v / data.length);
+		
+		This.noise = v;
+	});
 
 	this.loadScene(Scenario.scenes[Scenario.first_scene]);
 };
@@ -153,7 +168,7 @@ TextRPG.prototype.loadScene = function(scene){
 
 			var option = $('<div></div>').css({
 				"position": "relative"
-			});
+			}).attr('data-scene',s.scene);
 
 			if(num > 1){
 				option.css({
@@ -171,10 +186,74 @@ TextRPG.prototype.loadScene = function(scene){
 				"cursor": "pointer",
 				"margin-left": "10px"
 			});
+			
+			var meter_container = $('<div></div>').css({
+				"position": "relative",
+				"margin-top": "5px"
+			});
+			var meter = $('<div></div>').addClass('voiceometer').css({
+				"height": "20px",
+				"width": "0%",
+				"background": "#ff8080"
+			});
+			var meter_value = $('<div></div>').css({
+				"position": "absolute",
+				"height": "20px",
+				"top": "0",
+				"left": "0%",
+				"border-left": "solid #1ea7e1 3px"
+			}).attr('data-noise',0);
+			
+			meter_container.append(meter, meter_value);
 
-			option.append(number, option_text);
+			option.append(number, option_text, meter_container);
 
-			option.on('click', (function(scene_id){
+			option.on('click', (function(scene_id, meter, meter_value){
+				return function(){
+					var loops = 0;
+					
+					$('.voiceometer').css({
+						"width": "0%"
+					});
+					clearInterval(game.loop_function);
+					
+					meter_value.attr('data-noise',0);
+					
+					meter_value.css({
+						"left": "0%"
+					});
+					
+					game.loop_function = setInterval(function(){
+						loops++;
+						
+						var current_max_noise = meter_value.attr('data-noise');
+						
+						if(game.noise >= current_max_noise){
+							meter_value.attr('data-noise',game.noise);
+							
+							meter_value.css({
+								"left": (game.noise * 100) + "%"
+							});
+						}
+						
+						meter.css({
+								"width": (game.noise * 100) + "%"
+							});
+						
+						if(loops > 500){
+							clearInterval(game.loop_function);
+						
+							meter.css({
+								"width": "0%"
+							});
+						}
+					},10);
+					
+					//game.loadScene(Scenario.scenes[scene_id]);
+				}
+			})(s.scene, meter, meter_value));
+			
+			option.on('dblclick', (function(scene_id){
 				return function(){
 					game.loadScene(Scenario.scenes[scene_id]);
 				}
@@ -182,8 +261,45 @@ TextRPG.prototype.loadScene = function(scene){
 
 			options.append(option);
 		}
+		
+		var next_scene_button = $('<div></div>')
+		.css({
+			"text-align": "right"
+		})
+		.append(
+			$('<button></button>')
+				.html('Continue')
+				.css({
+					"position": "relative",
+					"right": "0",
+					"margin-top": "15px",
+					"background-color": "#1ea7e1",
+					"padding": "10px",
+					"border-radius": "5px",
+					"border": "#36bdf6 solid 2px",
+					"box-shadow": "#0684c7 0px 0px 1px 1px",
+					"color": "#fff",
+					"cursor": "pointer"
+				})
+		);
+		
+		next_scene_button.on('click',function(){
+			var max_noise = 0;
+			var next_scene = null;
+			
+			$('[data-noise]').each(function(){
+				var noise = $(this).attr('data-noise');
+				
+				if(noise >= max_noise){
+					max_noise = noise;
+					next_scene = $(this).closest('[data-scene]').attr('data-scene');
+				}
+			});
+			
+			game.loadScene(Scenario.scenes[next_scene]);
+		});
 
-		right_column.append(options);
+		right_column.append(options, next_scene_button);
 	} else {
 		var end = $('<div></div>').css({
 			"margin-top": "10px",
